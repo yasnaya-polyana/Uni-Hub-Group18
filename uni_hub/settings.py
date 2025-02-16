6e797ecd3937d74be9d64dbc8d688936afe991f6
Oscar Linehan <oladmin@Oscars-MacBook-Air.local>
2025-02-16 22:54:22 +0000

    Add password reset functionality

--- uni_hub/settings.py
+++ uni_hub/settings.py
@@ -39,17 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
-    # 3rd party
-    "accounts",
-    "rest_framework"
+    "accounts"
 ]
-
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ],
-}
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
@@ -133,13 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
-# STATIC_ROOT = BASE_DIR / "static"
-
-# STATICFILES_DIRS = [
-#     BASE_DIR / "/static",
-# ]
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- uni_hub/urls.py
+++ uni_hub/urls.py
@@ -30,6 +30,33 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
 
+    # Password Reset URLs
+    path('password_reset/', 
+         auth_views.PasswordResetView.as_view(
+             template_name='accounts/password_reset.html',
+             subject_template_name='registration/password_reset_subject.txt',
+             email_template_name='registration/password_reset_email.html'
+         ),
+         name='password_reset'),
+    
+    path('password_reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='accounts/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    
+    path('reset/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='accounts/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    
+    path('reset/done/', 
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='accounts/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
+
     # API
     path('api/', include('api.urls'))
 
