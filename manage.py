23cf4130efaf1d7151f7a1ef69461ac2c5158fab
Jack Palfrey <jackgpalfrey@outlook.com>
2025-02-28 23:17:00 +0000

    feat: add config.yaml

--- manage.py
+++ manage.py
@@ -3,11 +3,14 @@
 import os
 import sys
 
+from config import Config
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uni_hub.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uni_hub.settings")
     try:
+        Config.load()
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
@@ -18,5 +21,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- uni_hub/settings.py
+++ uni_hub/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 from django_jinja.builtins import DEFAULT_EXTENSIONS
 from dotenv import load_dotenv
 
+from config.Config import Config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
