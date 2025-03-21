1de1d3e5845015172bceb3c20a39ef729628884d
Cosmo Elford <cosmo2.elford@live.uwe.ac.uk>
2025-03-21 11:48:07 +0000

    bug fixes: back-end preprocessor for custom mentions

--- md_extensions/tailwind.py
+++ md_extensions/tailwind.py
@@ -1,15 +1,17 @@
-# extensions.py
+import re
 from markdown.extensions import Extension
 from markdown.treeprocessors import Treeprocessor
+from markdown.preprocessors import Preprocessor
 
-
+# Tailwind Extension
 class TailwindExtension(Extension):
     """An extension to add classes to tags"""
 
     def extendMarkdown(self, md):
+        md.preprocessors.register(MentionPreprocessor(md), "mention_preprocessor", 25)
         md.treeprocessors.register(TailwindTreeProcessor(md), "tailwind", 20)
 
-
+# Tailwind Preprocessor
 class TailwindTreeProcessor(Treeprocessor):
     """Walk the root node and modify any discovered tag"""
 
@@ -33,3 +35,22 @@ class TailwindTreeProcessor(Treeprocessor):
             tag_classes = self.classes.get(node.tag)
             if tag_classes:
                 node.attrib["class"] = tag_classes
+
+# Regex for Mention [@]
+MENTION_PATTERN = re.compile(r'\[@([a-zA-Z0-9_]+)\]')
+
+# Mention Preprocessor
+class MentionPreprocessor(Preprocessor):
+    def run(self, lines):
+        new_lines = []
+        for line in lines:
+            # Replace all [@username] with <a href="/p/username">@username</a>
+            line = MENTION_PATTERN.sub(self.mention_to_link, line)
+            new_lines.append(line)
+        return new_lines
+
+    def mention_to_link(self, match):
+        username = match.group(1)
+        return f'<a target="_blank" href="/u/{username}"  class="text-blue-600 bg-blue-100 font-medium px-1 py-0.5 rounded">@{username}</a>'
+
+# TODO: Add functionality for other mention types.
